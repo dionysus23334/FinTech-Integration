73fb58f9e54aef3d9391467accef18991a08b330
Guo Yuxi <134669199+dionysus23334@users.noreply.github.com>
2025-02-03 23:39:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,41 @@
 
+import streamlit as st
+
+st.set_page_config(page_title="Plotting Demo", page_icon="📈")
+
+# st.write(
+#     '''
+#     # FinTech Application
+#     '''
+# )
+
+st.html(
+    
+    """
+    <p style='font-weight:bold;; font-size:50px; text-align:center;'>FinTech Application</p>
+    <p style='font-size:25px; color:grey; text-align:center;'>Author: Yuxi Guo</p>
+    """
+    )
+
+st.write(
+    '''
+    Through the collaboration on spider, quantization and prompt engineering, 
+    this project proposes a system for financial information analysis and market prediction.
+    User can apply the various data on websites or input the keyword.
+    '''
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
