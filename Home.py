d6ed84b50f0a096e7467706ca2abc100f16e78d2
Guo Yuxi <134669199+dionysus23334@users.noreply.github.com>
2025-02-05 09:32:10 +0800

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.html(
+    
+    """
+    <p style='font-weight:bold;; font-size:50px; text-align:center;'>FinTech Application</p>
+    <p style='font-size:25px; color:grey; text-align:center;'>Author: Yuxi Guo</p>
+    """
+    )
+
+st.write(
+    '''
+    Through the collaboration on spider, quantization and prompt engineering, 
+    this project proposes a system for financial information analysis and market prediction.
+    User can apply the various data on websites or input the keyword.
+    '''
+)
