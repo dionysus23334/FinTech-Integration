dc8b1442aff220d4a6c90c8b55508a1a8f671494
Guo Yuxi <134669199+dionysus23334@users.noreply.github.com>
2025-02-05 12:30:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ pages_manager = st.navigation(
         st.Page("Home.py"),
         st.Page("SearchWeb.py"),
         st.Page("plot_df.py"),
-        st.Page("plot_demo.py"),
         st.Page("Word_Cloud.py")
         ]
     )
