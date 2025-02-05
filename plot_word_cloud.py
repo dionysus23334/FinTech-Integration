183f84e6e2d723b4ad444ee33e7ec6834c961e22
Guo Yuxi <134669199+dionysus23334@users.noreply.github.com>
2025-02-05 09:33:15 +0800

    Create plot_word_cloud.py

---
+++ plot_word_cloud.py
@@ -0,0 +1,25 @@
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import streamlit as st
+
+def generate_word_cloud(text_input):
+    # 创建词云对象
+    wordcloud = WordCloud(
+        font_path="simsun.ttc",
+        width=800, height=600,
+        background_color='white'
+    ).generate(text_input)
+
+    # 显示词云图片
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')  # 不显示坐标轴
+    return plt
+
+from data_collection.spider import SimpleSpider
+
+simplespider = SimpleSpider()
+data_example = simplespider.scrape_website("https://www.cls.cn/")
+
+plot = generate_word_cloud(data_example['text'])
+plot.show()
+st.pyplot(plot)
