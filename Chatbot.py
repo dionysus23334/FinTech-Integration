6ccd44288900ccfc62c5dad5074f56417d0b0ada
Guo Yuxi <134669199+dionysus23334@users.noreply.github.com>
2025-02-05 13:46:36 +0800

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -2,10 +2,11 @@ import streamlit as st
 import numpy as np
 
 # Insert a chat message container.
+with st.chat_message("Agent"):
+    st.write("Hello 👋")
+
 with st.chat_message("user"):
     st.write("Hello 👋")
-    st.line_chart(np.random.randn(30, 3))
-
 
 # Display a chat input widget inline.
 with st.container():
