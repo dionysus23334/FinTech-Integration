94c09bf948d8d9539d3f44fe1d8aee19a7a300ea
Guo Yuxi <134669199+dionysus23334@users.noreply.github.com>
2025-02-06 10:38:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ pages_manager = st.navigation(
     [
         st.Page("Home.py"),
         st.Page("Chatbot.py"),
-        st.Page("Word_Cloud.py")
+        st.Page("NLP_Analysis.py")
         ]
     )
 
