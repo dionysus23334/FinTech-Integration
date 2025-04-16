15169c617bffae25bc8af00fef6401fe46c1c3ef
Guo Yuxi <134669199+dionysus23334@users.noreply.github.com>
2025-04-16 21:52:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ pages_manager = st.navigation(
     [
         st.Page("Home.py"),
         st.Page("Chatbot.py"),
-        st.Page("NLP_Analysis.py")
+        st.Page("NLP_Analysis.py"),
+        st.Page("show_stock_df.py")
         ]
     )
 
