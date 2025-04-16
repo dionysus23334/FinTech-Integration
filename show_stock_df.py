c181b96fdf602f6d986a82732d2f17821461156d
Guo Yuxi <134669199+dionysus23334@users.noreply.github.com>
2025-04-16 22:08:21 +0800

    Update show_stock_df.py

--- show_stock_df.py
+++ show_stock_df.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from data_collection.spider import AutoSpider
 
-spider = AutoSpider()
+spider = AutoSpider().scrape_table_from_url()
 df = spider.get_dataframe()
 
 # 页面标题
