14a5a9b9cb46290d1b1b99e46ed2a893869130cf
Guo Yuxi <134669199+dionysus23334@users.noreply.github.com>
2025-04-19 20:41:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ pages_manager = st.navigation(
         st.Page("Stock_Data.py"),
         st.Page("RPS_And_STD.py"),
         st.Page("Events_And_Prices.py"),
+        st.Page("Momentum_App.py"),
         ]
     )
 
