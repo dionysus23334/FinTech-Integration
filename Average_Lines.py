db6189e16141b05b26165d72e8b7cefddbef80f3
Guo Yuxi <134669199+dionysus23334@users.noreply.github.com>
2025-04-20 12:52:18 +0800

    Update Average_Lines.py

--- Average_Lines.py
+++ Average_Lines.py
@@ -165,4 +165,8 @@ if uploaded_file is not None:
     # 可视化 P(t)
     st.subheader("📊 P(t) 及导数趋势")
     chart_data = stock_df.set_index("日期")[["P", "P_diff"]]
+    chart_data['零线'] = 0.0
+    chart_data.set_index('日期', inplace=True)
+
+
     st.line_chart(chart_data)
